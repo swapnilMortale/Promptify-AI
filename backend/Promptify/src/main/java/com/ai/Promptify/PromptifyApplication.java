021bef4f74eac065f440bb100e09bf6c74e94329
Swapnil <swapnilpatil75334@gmail.com>
2026-01-02 16:49:22 +0530

    Added backend and frontend code

---
+++ backend/Promptify/src/main/java/com/ai/Promptify/PromptifyApplication.java
@@ -0,0 +1,13 @@
+package com.ai.Promptify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromptifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromptifyApplication.class, args);
+	}
+
+}
---
+++ backend/Promptify/src/test/java/com/ai/Promptify/PromptifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.Promptify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromptifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
